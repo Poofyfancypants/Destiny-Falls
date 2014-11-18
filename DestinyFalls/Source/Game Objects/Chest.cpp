2d32e612774a8d24e463f96ba6d448ab90114654
Evan Doyle <evandoyle16@yahoo.com>
2014-11-18 15:56:31 -0500

    Chests and combat

--- DestinyFalls/Source/Game Objects/Chest.cpp
+++ DestinyFalls/Source/Game Objects/Chest.cpp
@@ -7,6 +7,13 @@
 
 Chest::Chest()
 {
+	if (rand() % 10 > 6)
+		trappedChest = true;
+	else
+		trappedChest = false;
+
+	numPots = rand() % 3;
+	numRunes = rand() % 2;
 }
 
 
@@ -71,7 +78,7 @@ void Chest::HandleCollision(const iObject* pOther)
 {
 	if (pOther->GetType() == OBJ_PLAYER)
 	{
-		if (Colliding == false)
+		if (Colliding == false && (numPots > 0 || numRunes > 0))
 		{
 			Colliding = true;
 			textTimer = 3.0f;
--- DestinyFalls/Source/Game States/CombatState.cpp
+++ DestinyFalls/Source/Game States/CombatState.cpp
@@ -34,7 +34,7 @@ void CombatState::Enter(void)
 	m_pObjects.push_back(player);
 	TurnIndex++;
 
-	for (unsigned int i = 0; i < 2; i++)
+	for (unsigned int i = 0; i < rand()%3+1; i++)
 	{
 		Object* temp = AddMinion();
 		m_pObjects.push_back(temp);
@@ -72,56 +72,52 @@ bool CombatState::Input(void)
 
 void CombatState::Update(float elapsedTime)
 {
-		for (size_t i = 0; i < m_pObjects.size(); i++)
+	for (size_t i = 0; i < m_pObjects.size(); i++)
+	{
+		if (((Player*)m_pObjects[0])->GetHealth() > 0)
 		{
-			if (((Player*)m_pObjects[0])->GetHealth() > 0)
-			{
 
-			}
-			else
-			{
-				((Player*)m_pObjects[0])->SetCombat(false);
-			}
-
-			for (size_t i = 0; i < 3; i++)
-			{
-				{
-
-				}
-			}
-
-
-			switch (m_pObjects[i]->GetType())
-			{
-			case Object::ObjectType::OBJ_PLAYER:
-			{
-												   if (((Player*)m_pObjects[i])->GetTurnPos() == CurrentTurn)
-												   {
-													   if (((Player*)m_pObjects[i])->TakeTurn())
-													   {
-
-													   }
-													   else
-													   {
-														   ((Player*)m_pObjects[i])->SetCombat(false);
-													   }
-													   CurrentTurn++;
-												   }
-			}
-				break;
-			case Object::ObjectType::OBJ_MINION:
-			{
-												   if (((Minion*)m_pObjects[i])->GetTurnPos() == CurrentTurn)
-												   {
-													   ((Minion*)m_pObjects[i])->TakeTurn();
-													   CurrentTurn++;
-												   }
-			}
-				break;
-			}
-			//CurrentTurn++;
 		}
-		//CurrentTurn = 0;
+		else
+		{
+			((Player*)m_pObjects[0])->SetCombat(false);
+		}
+
+		for (size_t i = 0; i < 3; i++)
+		{
+			{
+
+			}
+		}
+
+
+		switch (m_pObjects[i]->GetType())
+		{
+		case Object::ObjectType::OBJ_PLAYER:
+		{
+											   if (((Player*)m_pObjects[i])->GetTurnPos() == CurrentTurn)
+											   {
+												   if (((Player*)m_pObjects[i])->TakeTurn())
+												   {
+												   }
+													   CurrentTurn++; //Put this back in the check once player combat input is complete
+
+											   }
+		}
+			break;
+		case Object::ObjectType::OBJ_MINION:
+		{
+											   if (((Minion*)m_pObjects[i])->GetTurnPos() == CurrentTurn)
+											   {
+												   ((Minion*)m_pObjects[i])->TakeTurn();
+												   CurrentTurn++;
+											   }
+		}
+			break;
+		}
+		//CurrentTurn++;
+	}
+	//CurrentTurn = 0;
 }
 
 void CombatState::Render(void)
@@ -130,19 +126,39 @@ void CombatState::Render(void)
 
 	pGraphics->DrawRectangle(AbilityRect, SGD::Color{ 100, 150, 150, 150 });
 
-	pGraphics->DrawRectangle(Compation1HB, SGD::Color{ 100, 150, 150, 150 });
-	pGraphics->DrawRectangle(PlayerHB, SGD::Color{ 100, 150, 150, 150 });
-	pGraphics->DrawRectangle(Compation2HB, SGD::Color{ 100, 150, 150, 150 });
-	pGraphics->DrawRectangle(Enemy1HB, SGD::Color{ 100, 0, 255, 0 });
-	pGraphics->DrawRectangle(Enemy2HB, SGD::Color{ 100, 0, 255, 0 });
-	pGraphics->DrawRectangle(Enemy3HB, SGD::Color{ 100, 0, 255, 0 });
-
-	pGraphics->DrawRectangle(Compation1rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
-	pGraphics->DrawRectangle(Playerrect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
-	pGraphics->DrawRectangle(Compation2rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
-	pGraphics->DrawRectangle(Enemy1rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
-	pGraphics->DrawRectangle(Enemy2rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
-	pGraphics->DrawRectangle(Enemy3rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
+	for (size_t i = 0; i < m_pObjects.size(); i++)
+	{
+		if (i == 0)
+		{
+			pGraphics->DrawRectangle(Playerrect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
+			pGraphics->DrawRectangle(PlayerHB, SGD::Color{ 100, 150, 150, 150 });
+		}
+		if (i == 1)
+		{
+			pGraphics->DrawRectangle(Enemy1rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
+			pGraphics->DrawRectangle(Enemy1HB, SGD::Color{ 100, 0, 255, 0 });
+		}
+		if (i == 2)
+		{
+			pGraphics->DrawRectangle(Enemy2rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
+			pGraphics->DrawRectangle(Enemy2HB, SGD::Color{ 100, 0, 255, 0 });
+		}
+		if (i == 3)
+		{
+			pGraphics->DrawRectangle(Enemy3rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
+			pGraphics->DrawRectangle(Enemy3HB, SGD::Color{ 100, 0, 255, 0 });
+		}
+		if (i == 4)
+		{
+			pGraphics->DrawRectangle(Compation1HB, SGD::Color{ 100, 150, 150, 150 });
+			pGraphics->DrawRectangle(Compation1rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
+		}
+		if (i == 5)
+		{
+			pGraphics->DrawRectangle(Compation2rect, SGD::Color{ 100, 150, 150, 150 }, SGD::Color{ 255, 255, 255, 255 });
+			pGraphics->DrawRectangle(Compation2HB, SGD::Color{ 100, 150, 150, 150 });
+		}
+	}
 }
 
 Object* CombatState::AddMinion()
@@ -161,7 +177,7 @@ bool CombatState::DealDamage(int _DamType, Object* _this, int _target)
 	{
 	case CombatState::DamType::Melee:
 	{
-										if (m_pObjects[_target]->GetType()==iObject::OBJ_PLAYER)
+										if (m_pObjects[_target]->GetType() == iObject::OBJ_PLAYER)
 										{
 											((Player*)m_pObjects[_target])->SetHealth(((Player*)m_pObjects[_target])->GetHealth() - 20);
 										}
