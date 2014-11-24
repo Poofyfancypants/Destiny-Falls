59cc0c425866c0e017493230c2dd4a3b69e9c02a
YvonneNeuland <YvonneNeuland@fullsail.edu>
2014-11-24 15:53:36 -0500

    Added BitmapFontManager.h and BitmapFontManager.cpp

---
+++ DestinyFalls/Source/Managers/BitmapFontManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "../Bitmap Font/BitmapFont.h"
+#include <string>
+#include <map>
+using namespace std;
+
+class BitmapFontManager
+{
+
+	map<string , BitmapFont*> LoadedFonts;
+	//*******************************************************************
+	// SINGLETON!
+	//	- static member to hold the singleton object
+	//	- prevent access to constructors / destructor / =op
+	static BitmapFontManager* s_pInstance;
+public:
+	BitmapFontManager() = default;
+	~BitmapFontManager() = default;
+
+	//*******************************************************************
+	// SINGLETON!
+	//	- static accessor to get the singleton object
+	static BitmapFontManager* GetInstance();
+	static void DeleteInstance();
+
+	void Render(string fontName, const char* output , SGD::Point position, float scale, SGD::Color color );
+	void Load( string fontName, string fileName );
+
+	void AddToBitmapFontMap( BitmapFont* font );
+
+	int CheckSize();
+};
+
