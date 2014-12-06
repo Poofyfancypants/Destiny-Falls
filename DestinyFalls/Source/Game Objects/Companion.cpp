02257835c8f6feb5c6c4bf7141748489f1ccea6a
YvonneNeuland <YvonneNeuland@fullsail.edu>
2014-12-06 13:08:09 -0500

    Added TakeTurn switch

--- DestinyFalls/Source/Game Objects/Companion.cpp
+++ DestinyFalls/Source/Game Objects/Companion.cpp
@@ -17,7 +17,7 @@ void Companion::Update( float elapsedTime )
 {
 	if( m_bUpdateAnimation )
 	{
-		m_pAnimator->GetInstance()->GetInstance()->Update( *this->GetTimeStamp(), elapsedTime );
+		m_pAnimator->GetInstance()->GetInstance()->Update( *this->GetTimeStamp() , elapsedTime );
 	}
 }
 
@@ -26,9 +26,9 @@ void Companion::Render()
 	SGD::GraphicsManager* pGraphics = SGD::GraphicsManager::GetInstance();
 
 
-	SGD::Vector vec = { ( m_ptPosition.x ), ( m_ptPosition.y ) };
-	SGD::Point point = { vec.x - GameplayState::GetInstance()->GetWorldCam().x, vec.y - GameplayState::GetInstance()->GetWorldCam().y };
-	pGraphics->DrawTexture( m_hImage, point );
+	SGD::Vector vec = { ( m_ptPosition.x ) , ( m_ptPosition.y ) };
+	SGD::Point point = { vec.x - GameplayState::GetInstance()->GetWorldCam().x , vec.y - GameplayState::GetInstance()->GetWorldCam().y };
+	pGraphics->DrawTexture( m_hImage , point );
 
 }
 
@@ -42,23 +42,49 @@ void Companion::CombatRender( int _posIndex )
 			if( m_pAnimator->GetInstance()->CheckSize() )
 			{
 				m_pAnimator->GetInstance()->Render( *this->GetTimeStamp() , Companion1rect.right , Companion1rect.bottom );
-			}			
+			}
 			break;
 		case 1:
 			if( m_pAnimator->GetInstance()->CheckSize() )
 			{
 				m_pAnimator->GetInstance()->Render( *this->GetTimeStamp() , Companion2rect.right , Companion2rect.bottom );
-			}			
+			}
 			break;
-		
+
 		default:
 			break;
 	}
 }
 
-bool Companion::TakeTurn(float elapsedTime)
+bool Companion::TakeTurn( float elapsedTime )
 {
 	CombatState* pCombat = CombatState::GetInstance();
+
+	switch( GetCoType() )
+	{
+		case Cleric:
+		{
+
+		}
+			break;
+		case Melee:
+		{
+
+		}
+			break;
+		case Mage:
+		{
+
+		}
+			break;
+		case Tank:
+		{
+
+		}
+			break;
+		default:
+			break;
+	}
 
 	m_bUpdateAnimation = true;
 	this->GetTimeStamp()->SetCurrentFrame( 0 );
@@ -73,20 +99,20 @@ void Companion::SetCompanionAnimation( int companionType )
 
 	switch( companionType )
 	{
-	case 1:
-		this->GetTimeStamp()->SetCurrentAnimation( "ClericAttack" );
-		break;
-	case 2:
-		this->GetTimeStamp()->SetCurrentAnimation( "Companion1Attack" );
-		break;
-	case 3:
-		this->GetTimeStamp()->SetCurrentAnimation( "RangerAttack" );
-		break;
-	case 4:
-		this->GetTimeStamp()->SetCurrentAnimation( "RangerAttack" );
-		break;
-	default:
-		break;
+		case 1:
+			this->GetTimeStamp()->SetCurrentAnimation( "ClericAttack" );
+			break;
+		case 2:
+			this->GetTimeStamp()->SetCurrentAnimation( "Companion1Attack" );
+			break;
+		case 3:
+			this->GetTimeStamp()->SetCurrentAnimation( "RangerAttack" );
+			break;
+		case 4:
+			this->GetTimeStamp()->SetCurrentAnimation( "RangerAttack" );
+			break;
+		default:
+			break;
 	}
 
 	this->GetTimeStamp()->SetCurrentFrame( 0 );
